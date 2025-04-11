54eabb74e70febdc4f457a3ba5cb37d377f276e4
MealWithoutSoup <108072455+MealWithoutSoup@users.noreply.github.com>
2025-04-11 22:32:52 +0900

    디렉토리 수정

--- JaeHong/Week1/src/test2.java
+++ JaeHong/Week1/src/test2.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class test1 {
+public class test2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int i = sc.nextInt();
